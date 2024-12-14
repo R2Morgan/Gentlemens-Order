83d7441c7632a36b86e9f378d0cb02619d12b6ec
r2morgan <stefandobrescu2008@yahoo.com>
2024-12-14 19:04:30 +0200

    v.0.0.2 - Basic Structure
    - add basic project structure & basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./components/home-page/home-page.component";
+import {ArticlePageComponent} from "./components/article-page/article-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'articles', component: ArticlePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import { HeaderComponent } from './components/shared/header/header.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ArticlePageComponent } from './components/article-page/article-page.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent,
+    ArticlePageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MatSlideToggleModule
+    AppRoutingModule
   ],
   providers: [
     provideAnimationsAsync()
