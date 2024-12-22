6b800666e2efc43f621d996961f3e303a13390ec
r2morgan <stefandobrescu2008@yahoo.com>
2024-12-22 12:36:36 +0200

    v.0.0.3 - Header & Footer
    - fix scss configuration
    - finish project structure
    - add header component
    - add footer component

--- src/app/components/article-page/article-page.component.ts
+++ src/app/components/article-page/article-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'article-page',
   templateUrl: './article-page.component.html',
-  styleUrl: './article-page.component.css'
+  styleUrl: './article-page.component.scss'
 })
 export class ArticlePageComponent {
 
--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  // DOM properties
+  lastScrollTop = 0;
+
+  // DOM functionality
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScroll = document.documentElement.scrollTop;
+    const navbar = document.querySelector('.navbar') as HTMLElement;
+    const scrollThreshold = 1000;
+
+    if (currentScroll > scrollThreshold) {
+      if (currentScroll > this.lastScrollTop) {
+        navbar.classList.add('navbar-hidden'); // Add class to hide navbar
+      } else {
+        navbar.classList.remove('navbar-hidden'); // Remove class to show navbar
+      }
+    }
+    this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Prevent negative values
+  }
 }
